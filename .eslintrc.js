9c5fbd683ef713d7cc7fa5bab2ec722639630a4f
Ivanych <lvanych@mail.ru>
2024-03-21 14:17:52 +0400

    fix4 commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,26 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true
+  "env": {
+      "browser": true,
+      "es2021": true
   },
-  'extends': 'eslint:recommended',
-  'overrides': [
-    {
-      'files': ['**/*/*.test.js'],
-      'plugins': ['jest'],
-      'extends': ['plugin:jest/recommended'],
-      'rules': { 'jest/prefer-expect-assertions': 'off' }
-    }
+  "extends": "eslint:recommended",
+  "overrides": [
+      {
+          "env": {
+              "node": true
+          },
+          "files": [
+              ".eslintrc.{js,cjs}"
+          ],
+          "parserOptions": {
+              "sourceType": "script"
+          }
+      }
   ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
   },
-  'rules': {}
+  "rules": {
+  }
 }
