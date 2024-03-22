30ef856c43c1993e366304cdcdf2fa2a3fe29c25
Ivanych <lvanych@mail.ru>
2024-03-22 07:42:44 +0400

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   'env': {
     'browser': true,
     'es2021': true,
-    'node': true
+    'node': true,
+    "jest/globals": true
   },
   'extends': 'eslint:recommended',
   'overrides': [
@@ -17,5 +18,6 @@ module.exports = {
     'ecmaVersion': 'latest',
     'sourceType': 'module'
   },
-  'rules': {}
+  'rules': {
+  }
 }
